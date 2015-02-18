e2d67d25dd885a128f065aa6b6228a01e9e9968e
James Maynard & monty rohde <student@epicodus.com>
2015-02-18 15:40:29 -0800

    Take a purchase order for a group of people rather than an individual.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 var MovieTicket = {
 
   price: 0,
-  calculatePrice: function(title, day, timeOfDay, age) {
+  orderPrice:0,
+
+  calculatePrice: function(movie, day, timeOfDay, child, student, adult, senior) {
 
     if (day === "Monday" || "Tuesday" || "Wednesday" || "Thursday") {
       var costDay = 2.00
@@ -10,25 +12,54 @@ var MovieTicket = {
     } else  {
       costDay = 3.00
     }
+    this.price += costDay;
 
     if (timeOfDay === "Matinee") {
       var costTimeOfDay = 0
     } else { costTimeOfDay = 3.00
 
     }
+    this.price += costTimeOfDay;
 
-    if (age === "Child") {
-      var costAge = 0
-    } else if (age === "Adult") {
-      costAge = 2.00
-    } else if (age === "Student") {
-      costAge = 1.50
-    } else { costAge = 1.00
-
-    }
+    this.orderPrice += (child*(this.price));
+    this.orderPrice += (student*(this.price+1.5));
+    this.orderPrice += (adult*(this.price + 2));
+    this.orderPrice += (senior*(this.price + 1));
 
 
-   return this.price + costDay + costTimeOfDay + costAge;
+    // if (age === "Child") {
+    //   var costAge = 0
+    // } else if (age === "Adult") {
+    //   costAge = 2.00
+    // } else if (age === "Student") {
+    //   costAge = 1.50
+    // } else { costAge = 1.00
+    //
+    // }
+
+
+   return this.orderPrice;
   }
 
 };
+
+$(document).ready(function(event) {
+
+  $('form#orderTicket').submit(function(event) {
+    var movie = $('select#movie').val();
+    var day = $('select#day').val();
+    var timeOfDay = $('select#timeOfDay').val();
+    var child= parseInt($('input#child').val());
+    var student= parseInt($('input#student').val());
+    var adult= parseInt($('input#adult').val());
+    var senior= parseInt($('input#senior').val());
+
+    // var age = $('select#age').val();
+
+    var movieTicket = Object.create(MovieTicket);
+
+    $("ul.purchasedTickets").append("<li><span class='ticket'>" + "$" + movieTicket.calculatePrice(movie, day, timeOfDay, child, student, adult, senior) + "</span></li>");
+    event.preventDefault();
+  });
+
+});
--- spec/specs.js
+++ spec/specs.js
@@ -2,7 +2,11 @@ describe("MovieTicket", function() {
   describe('price', function() {
     it('will output a price given four perameters', function() {
       var testMovieTicket = Object.create(MovieTicket);
-      expect(testMovieTicket.calculatePrice("Title", "Monday", "Matinee", "Student")).to.equal(3.50);
+      expect(testMovieTicket.calculatePrice("Title", "Monday", "Matinee", 0,1,0,0)).to.equal(3.50);
+    });
+    it('will output a price for a family of four, 2 adults and 2 children', function() {
+      var testMovieTicket =Object.create(MovieTicket);
+      expect(testMovieTicket.calculatePrice('Title','Monday','Matinee', 2, 0, 2, 0)).to.equal(12);
     });
   });
 
